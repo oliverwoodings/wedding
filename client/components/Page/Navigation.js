4720e07eb99b8a84345a05f52333f476ee957df2
Oliver Woodings <oliver.woodings@gmail.com>
2018-08-24 09:00:23 +0100

    Tweak the nav layout

--- client/components/Page/Navigation.js
+++ client/components/Page/Navigation.js
@@ -13,22 +13,20 @@ const ITEMS = {
 export default function Navigation ({ path }) {
   return (
     <div className={styles.container}>
-      <div className={styles.navigation}>
-        {Object.keys(ITEMS).map((name) => {
-          const isActive = path === ITEMS[name]
-          return (
-            <a
-              href={isActive ? null : ITEMS[name]}
-              key={name}
-              className={classnames(styles.item, {
-                [styles.activeItem]: isActive
-              })}
-            >
-              {name}
-            </a>
-          )
-        })}
-      </div>
+      {Object.keys(ITEMS).map((name) => {
+        const isActive = path === ITEMS[name]
+        return (
+          <a
+            href={isActive ? null : ITEMS[name]}
+            key={name}
+            className={classnames(styles.item, {
+              [styles.activeItem]: isActive
+            })}
+          >
+            {name}
+          </a>
+        )
+      })}
     </div>
   )
 }
