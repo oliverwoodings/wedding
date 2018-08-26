0737d8c765a208ee8807a1812502da450f2cd051
Oliver Woodings <oliver.woodings@gmail.com>
2018-08-26 14:35:08 +0100

    Stuff

--- client/pages/Logistics/Logistics.js
+++ client/pages/Logistics/Logistics.js
@@ -24,13 +24,13 @@ export default function Logistics ({ user }) {
         <P>The nearest mainline train stations are Ivybridge, Totnes, Plymouth and Exeter, all of which have direct trains from London. It is a good idea to book your onward taxi in advance (there is no Uber down here!).</P>
       </Block>
       <Block header='Accomodation'>
-        <P>Ringmore itself has very limited accomodation, so we advise looking slightly further afield. The villages of Challaborough, Kingston, Modbury, Bigbury and St. Ann's Chapel are all within a 10 minute drive. If you are looking for a larger town to base yourself in then Kingsbridge or Ivybridge are both lovely.</P>
+        <P>Ringmore itself has very limited accomodation, so we advise looking slightly further afield. The villages of Challaborough, Kingston, Modbury, Bigbury and St. Ann's Chapel are all within a 10 minute drive. If you are looking for a larger town to base yourself in then Kingsbridge or Ivybridge are both lovely. You can find holiday cottages, BnBs and hotels in all of these places via websites such as <Link href='https://www.airbnb.co.uk/'>Airbnb</Link> and <Link href='https://www.booking.com/'>Booking.com</Link>.</P>
         <P>The nearest static caravan holiday park can be found in <Link href='http://www.parkdeanholidays.co.uk/devon-holidays/challaborough-bay/challaborough-bay-holiday-park.htm'>Challaborough Park Dean</Link> (10 minute walk to Ringmore), while the nearest camping and caraving ground is <Link href='http://www.pennymoor-camping.co.uk/'>Pennymoor</Link> in Modbury.</P>
       </Block>
       <Block header='On the day'>
         <InviteType eveningOnly={user.eveningOnly} />
         <P>Parking will be available in a nearby field in Ringmore. We'll update you once we have a location confirmed! There will also be signage on the day. Please <Strong>do not</Strong> use the pub car park (opposite the church) or the National Trust car park in the village.</P>
-        <P>All activities for the day are based within Ringmore in easy walking distance, so no need to worry about taxis to the reception!</P>
+        {!user.eveningOnly && <P>All activities for the day are based within Ringmore in easy walking distance, so no need to worry about taxis to the reception!</P>}
       </Block>
       <Block header='Map'>
         <iframe
