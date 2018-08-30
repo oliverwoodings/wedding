1e400d9eb6fabed57ce734614666d51fca66fcad
Oliver Woodings <oliver.woodings@gmail.com>
2018-08-30 21:49:52 +0100

    Quick music restructure

--- app/client/pages/Music/Music.js
+++ app/client/pages/Music/Music.js
@@ -1,60 +1,14 @@
-import React, { Component } from 'react'
-import { debounce, get } from 'lodash-es'
-import createPlayer from 'web-audio-player'
-import withQuery from '../../lib/withQuery'
+import React from 'react'
 import PageBody from '../../components/PageBody'
-import SearchSpotifyQuery from './SearchSpotify.graphql'
+import { Paragraph as P, Link } from '../../components/typography'
+import SearchSpotify from './SearchSpotify'
+import styles from './Music.css'
 
-class Music extends Component {
-  state = {
-    query: ''
-  }
-
-  debouncedSearch = debounce(this.props.searchSpotify.execute, 300)
-
-  render () {
-    const { user, searchSpotify } = this.props
-    const tracks = get(searchSpotify, 'data.tracks.items', [])
-    return (
-      <PageBody title='Music' prev='/photos'>
-        <input
-          onChange={::this.onSearch}
-          value={this.state.query}
-        />
-        {tracks.map((track) => (
-          <div
-            key={track.id}
-            onClick={() => this.playTrack(track)}
-          >
-            {track.name}
-          </div>
-        ))}
-      </PageBody>
-    )
-  }
-
-  onSearch ({ target }) {
-    this.setState({ query: target.value })
-    this.debouncedSearch({
-      query: target.value
-    })
-  }
-
-  playTrack (track) {
-    if (this.audio) {
-      this.audio.stop()
-    }
-    const url = '/preview/' + encodeURIComponent(track.previewUrl)
-    const audio = createPlayer(url)
-    audio.on('load', () => {
-      audio.play()
-      audio.node.connect(audio.context.destination)
-    })
-    this.audio = audio
-  }
+export default function Music () {
+  return (
+    <PageBody title='Music' prev='/photos' className={styles.body}>
+      <P>The fantastic <Link href='http://intuxicated.co.uk'>Intuxicated</Link> will be entertaining us with live music throughout the evening. For the rest of the day (and night!), we need your help! Give us a hand by adding your favourite songs to our Spotify playlist:</P>
+      <SearchSpotify />
+    </PageBody>
+  )
 }
-
-export default withQuery(SearchSpotifyQuery, {
-  lazy: true,
-  name: 'searchSpotify'
-})(Music)
---
+++ app/client/pages/Music/SearchSpotify.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { debounce, get } from 'lodash-es'
+import createPlayer from 'web-audio-player'
+import withQuery from '../../lib/withQuery'
+import SearchSpotifyQuery from './SearchSpotify.graphql'
+
+class SearchSpotify extends Component {
+  state = {
+    query: ''
+  }
+
+  debouncedSearch = debounce(this.props.searchSpotify.execute, 300)
+
+  render () {
+    const { user, searchSpotify } = this.props
+    const tracks = get(searchSpotify, 'data.tracks.items', [])
+    return (
+      <div>
+        <input
+          onChange={::this.onSearch}
+          value={this.state.query}
+        />
+        {tracks.map((track) => (
+          <div
+            key={track.id}
+            onClick={() => this.playTrack(track)}
+          >
+            {track.name}
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  onSearch ({ target }) {
+    this.setState({ query: target.value })
+    this.debouncedSearch({
+      query: target.value
+    })
+  }
+
+  playTrack (track) {
+    if (this.audio) {
+      this.audio.stop()
+    }
+    const url = '/preview/' + encodeURIComponent(track.previewUrl)
+    const audio = createPlayer(url)
+    audio.on('load', () => {
+      audio.play()
+      audio.node.connect(audio.context.destination)
+    })
+    this.audio = audio
+  }
+}
+
+export default withQuery(SearchSpotifyQuery, {
+  lazy: true,
+  name: 'searchSpotify'
+})(SearchSpotify)
